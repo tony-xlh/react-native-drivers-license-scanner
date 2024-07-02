65bd3efc49e4317fda22bd0e11ca731fa777d380
xulihang <xulihanghai@163.com>
2024-07-02 11:32:58 +0800

    add react-native-worklets-core to enable frame processor

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,21 @@
 import React from 'react';
 import {SafeAreaView, StyleSheet, Button} from 'react-native';
+import * as DBR from 'vision-camera-dynamsoft-barcode-reader';
+import * as DDN from 'vision-camera-dynamsoft-document-normalizer';
 
 function App(): React.JSX.Element {
+  React.useEffect(() => {
+    (async () => {
+      const DBRLicenseResult = await DBR.initLicense(
+        'DLS2eyJoYW5kc2hha2VDb2RlIjoiMjAwMDAxLTE2NDk4Mjk3OTI2MzUiLCJvcmdhbml6YXRpb25JRCI6IjIwMDAwMSIsInNlc3Npb25QYXNzd29yZCI6IndTcGR6Vm05WDJrcEQ5YUoifQ==',
+      );
+      const DDNLicenseResult = await DDN.initLicense(
+        'DLS2eyJoYW5kc2hha2VDb2RlIjoiMjAwMDAxLTE2NDk4Mjk3OTI2MzUiLCJvcmdhbml6YXRpb25JRCI6IjIwMDAwMSIsInNlc3Npb25QYXNzd29yZCI6IndTcGR6Vm05WDJrcEQ5YUoifQ==',
+      );
+      console.log(DBRLicenseResult);
+      console.log(DDNLicenseResult);
+    })();
+  }, []);
   return (
     <SafeAreaView style={styles.container}>
       <Button title="Read Barcodes from the Camera" />
