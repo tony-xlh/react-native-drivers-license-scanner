d61f00725f703c4b66572cbb104bb93a5300d32d
xulihang <xulihanghai@163.com>
2024-07-02 13:27:18 +0800

    add back handler

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import {SafeAreaView, StyleSheet, Button, View, Text} from 'react-native';
+import {SafeAreaView, StyleSheet, Button, View, Text, BackHandler} from 'react-native';
 import * as DBR from 'vision-camera-dynamsoft-barcode-reader';
 import * as DDN from 'vision-camera-dynamsoft-document-normalizer';
 import DLScanner from './components/DLScanner';
@@ -7,6 +7,21 @@ import DLScanner from './components/DLScanner';
 function App(): React.JSX.Element {
   const [isScanning, setIsScanning] = React.useState(false);
   React.useEffect(() => {
+    BackHandler.addEventListener('hardwareBackPress', function () {
+      if (!isScanning) {
+        /**
+         * When true is returned the event will not be bubbled up
+         * & no other back action will execute
+         */
+        setIsScanning(false);
+        return true;
+      }
+      /**
+       * Returning false will let the event to bubble up & let other event listeners
+       * or the system's default back action to be executed.
+       */
+      return false;
+    });
     (async () => {
       const DBRLicenseResult = await DBR.initLicense(
         'DLS2eyJoYW5kc2hha2VDb2RlIjoiMjAwMDAxLTE2NDk4Mjk3OTI2MzUiLCJvcmdhbml6YXRpb25JRCI6IjIwMDAwMSIsInNlc3Npb25QYXNzd29yZCI6IndTcGR6Vm05WDJrcEQ5YUoifQ==',
