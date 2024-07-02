88f51c25f1dd8048d4611f5ffc79a96a312c06e8
xulihang <xulihanghai@163.com>
2024-07-02 14:46:36 +0800

    use react-native-svg to draw the overlay

--- src/components/DLScanner.tsx
+++ src/components/DLScanner.tsx
@@ -1,5 +1,6 @@
-import React from 'react';
-import {StyleSheet} from 'react-native';
+/* eslint-disable react-hooks/exhaustive-deps */
+import React, {useState} from 'react';
+import {Dimensions, Platform, StyleSheet} from 'react-native';
 import {
   Camera,
   runAsync,
@@ -8,15 +9,68 @@ import {
   useFrameProcessor,
 } from 'react-native-vision-camera';
 import * as DDN from 'vision-camera-dynamsoft-document-normalizer';
+import {Svg, Polygon} from 'react-native-svg';
+import {DetectedQuadResult} from 'vision-camera-dynamsoft-document-normalizer';
+import {Worklets, useSharedValue} from 'react-native-worklets-core';
+import {getRectFromPoints} from '../Utils';
 
 function DLScanner(): React.JSX.Element {
-  const [hasPermission, setHasPermission] = React.useState(false);
-  const [isActive, setIsActive] = React.useState(false);
+  const [hasPermission, setHasPermission] = useState(false);
+  const [isActive, setIsActive] = useState(false);
+  const [pointsText, setPointsText] = useState('default');
+  const [viewBox, setViewBox] = useState('0 0 1080 1920');
+  const [detectionResults, setDetectionResults] = useState(
+    [] as DetectedQuadResult[],
+  );
   const device = useCameraDevice('back');
   const cameraFormat = useCameraFormat(device, [
-    {videoResolution: {width: 1280, height: 720}},
-    {fps: 60},
+    { videoAspectRatio: 16 / 9 },
+    { videoResolution: { width: 1920, height: 1080 } },
+    { fps: 60 },
   ]);
+  const frameWidth = useSharedValue(1920);
+  const frameHeight = useSharedValue(1080);
+  const convertAndSetResults = (
+    records: Record<string, DetectedQuadResult>,
+  ) => {
+    let results: DetectedQuadResult[] = [];
+    for (let index = 0; index < Object.keys(records).length; index++) {
+      const result = records[Object.keys(records)[index]];
+      const rect = getRectFromPoints(result.location.points);
+      if (rect.width / getFrameSize()[0].value < 0.95) {
+        //avoid full screen misdetection
+        results.push(result);
+      }
+    }
+    setDetectionResults(results);
+  };
+  const getFrameSize = () => {
+    let width, height;
+    if (Platform.OS === 'android') {
+      if (
+        frameWidth > frameHeight &&
+        Dimensions.get('window').width > Dimensions.get('window').height
+      ) {
+        width = frameWidth;
+        height = frameHeight;
+      } else {
+        console.log('Has rotation');
+        width = frameHeight;
+        height = frameWidth;
+      }
+    } else {
+      width = frameWidth;
+      height = frameHeight;
+    }
+    return [width, height];
+  };
+
+  const updateViewBox = () => {
+    const frameSize = getFrameSize();
+    setViewBox('0 0 ' + frameSize[0] + ' ' + frameSize[1]);
+    console.log('viewBox' + viewBox);
+  };
+  const convertAndSetResultsJS = Worklets.createRunOnJS(convertAndSetResults);
   React.useEffect(() => {
     (async () => {
       const status = await Camera.requestCameraPermission();
@@ -24,7 +78,29 @@ function DLScanner(): React.JSX.Element {
       setIsActive(true);
     })();
   }, []);
-  // eslint-disable-next-line prettier/prettier
+
+  React.useEffect(() => {
+    updateViewBox();
+    updatePointsData();
+  }, [detectionResults]);
+
+  const updatePointsData = () => {
+    if (detectionResults.length > 0) {
+      let result = detectionResults[0];
+      if (result) {
+        let location = result.location;
+        let pointsData = location.points[0].x + ',' + location.points[0].y + ' ';
+        pointsData = pointsData + location.points[1].x + ',' + location.points[1].y + ' ';
+        pointsData = pointsData + location.points[2].x + ',' + location.points[2].y + ' ';
+        pointsData = pointsData + location.points[3].x + ',' + location.points[3].y;
+        console.log(pointsData);
+        setPointsText(pointsData);
+      }
+    } else {
+      setPointsText('default');
+    }
+  };
+
   const frameProcessor = useFrameProcessor((frame) => {
     'worklet';
     console.log('detect frame');
@@ -33,6 +109,7 @@ function DLScanner(): React.JSX.Element {
       try {
         const results = DDN.detect(frame);
         console.log(results);
+        convertAndSetResultsJS(results);
       } catch (error) {
         console.log(error);
       }
@@ -50,6 +127,19 @@ function DLScanner(): React.JSX.Element {
             frameProcessor={frameProcessor}
             pixelFormat="yuv"
           />
+          <Svg
+            style={StyleSheet.absoluteFill}
+            viewBox={viewBox}>
+            {pointsText !== 'default' && (
+              <Polygon
+                points={pointsText}
+                fill="lime"
+                stroke="green"
+                opacity="0.5"
+                strokeWidth="1"
+              />
+            )}
+          </Svg>
         </>
       )}
     </>
